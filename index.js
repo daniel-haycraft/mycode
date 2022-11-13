92adf41d1813739c144e99544165db1b6a299737
Daniel Haycraft <dhcopy1@gmail.com>
2022-11-12 17:55:45 -0700

    yf

---
+++ index.js
@@ -0,0 +1,11 @@
+let arr = [1, 2, 3, 4, 5]
+        // 0  1  2  3  4
+let groceries = ['cherrios', 'chicken', 'steak', 'other yummies']
+let splitZero = groceries[1].split('')
+function yoYo(array){
+  for(let i = 0; array.length > i; i++){
+    console.log(array[i])
+  }
+}
+
+yoYo(arr)
